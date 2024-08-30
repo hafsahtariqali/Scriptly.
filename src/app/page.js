7eca14746548c1a79b0b95d3932662f5290793f3
arushisingh2803 <124163610+arushisingh2803@users.noreply.github.com>
2024-08-30 01:39:54 +0100

    groq folder strucrure changed

--- src/app/page.js
+++ src/app/page.js
@@ -6,28 +6,6 @@ import WhyUs from "@/components/WhyUs";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
-import Groq from "groq-sdk";
-
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
-
-export async function main() {
-  const chatCompletion = await getGroqChatCompletion();
-  console.log(chatCompletion.choices[0]?.message?.content || "");
-}
-
-export async function getGroqChatCompletion() {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: "You are an assistant for a Youtuber. You will create scripts according to the users requirements such as their channel type, their demographic, length of video, etc. You will take these into account while creating the script. The script has to be made according to how long it will take to be verbally spoken out. You will take the users preferences and use them as inspiration. You will make scripts which align with the Youtube algorithm. You will allow for regenrations with specific requests.",
-      },
-    ],
-    model: "llama3-8b-8192",
-  });
-}
-
-
 export default function Home() {
   return (
     <>
