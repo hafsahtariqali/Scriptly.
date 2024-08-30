49190db58be9d3a10e8cb884122209dd1bad8353
Hafsa Tariq Ali <137608362+hafsahtariqali@users.noreply.github.com>
2024-08-30 14:52:20 +0500

    mobilemenu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -27,16 +27,20 @@ const navItems = [
 const MobileMenu = () => {
   const [navOpen, setNavOpen] = useState(false);
 
+  const handleLinkClick = () => {
+    setNavOpen(false); // Close the menu when a nav link is clicked
+  };
+
   return (
     <div className="block md:hidden">
       {!navOpen ? (
         <button onClick={() => setNavOpen(true)} className="flex justify-center">
-          <Menu size={29} color="#f7a8a8"/>
+          <Menu size={29} color="#f7a8a8" />
         </button>
       ) : (
         <>
           <button onClick={() => setNavOpen(false)} className="flex justify-center">
-            <X size={29} color="#f7a8a8"/>
+            <X size={29} color="#f7a8a8" />
           </button>
           <div className="fixed top-20 left-0 w-full bg-transparent backdrop-blur-lg border-b border-t z-50 max-h-screen overflow-y-auto">
             <ul className="flex flex-col items-center py-8 space-y-4">
@@ -44,6 +48,7 @@ const MobileMenu = () => {
                 <li key={index}>
                   <a
                     href={item.url}
+                    onClick={handleLinkClick} // Trigger the menu close on click
                     className="block text-[#f7a8a8] hover:text-gray-400 p-4"
                   >
                     {item.title}
